207bd2e811ed8cd6f8e21edd5eaee3693d8c2b49
vikas kumar gupta <mrvikaskumargupta@gmail.com>
2021-03-03 11:32:41 +0530

    added demo project to local repo

---
+++ src/com/vikas/test/Welcome.java
@@ -0,0 +1,14 @@
+package com.vikas.test;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+		String fullName = "vikas kumar gupta";
+		if (fullName.startsWith("vikas")) {
+			System.out.println("welcome vikas! how are you");
+		} else {
+			System.out.println("not vikas");
+		}
+	}
+}
